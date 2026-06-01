946d3960c15a667a75dffdd1752497b3eb625922
harshad8623 <dhuppeh@gmail.com>
2026-06-01 11:52:41 +0530

    Auto-update on 01-06-2026 at 11:52:41.27

---
+++ 1_Day/18_program.py
@@ -0,0 +1,5 @@
+# Print type of True, 3.14, 'hello', 42
+print(f"The type of True is: {type(True)}")
+print(f"The type of 3.14 is: {type(3.14)}")
+print(f"The type of 'hello' is: {type('hello')}")
+print(f"The type of 42 is: {type(42)}")
