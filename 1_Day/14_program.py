add63f52aa29182fe43d1a8b812b03975f79c652
harshad8623 <dhuppeh@gmail.com>
2026-06-01 11:47:34 +0530

    Auto-update on 01-06-2026 at 11:47:34.60

---
+++ 1_Day/14_program.py
@@ -0,0 +1,8 @@
+# Concatenate first name and last name
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+full_name = f"{f_name} {l_name}"
+print(f"Your full name is: {full_name}")    
+
+# or
+# full_name = f_name + " " + l_name
