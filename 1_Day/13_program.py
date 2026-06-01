a007a0c833314ab3b1773149165ab763d58d3808
harshad8623 <dhuppeh@gmail.com>
2026-06-01 11:46:21 +0530

    Auto-update on 01-06-2026 at 11:46:21.65

---
+++ 1_Day/13_program.py
@@ -0,0 +1,7 @@
+# Print the ASCII value of any character
+char = input("Enter a character: ")
+if len(char) == 1:
+    ascii_value = ord(char)
+    print(f"The ASCII value of '{char}' is: {ascii_value}")
+else:
+    print("Please enter a single character.")
