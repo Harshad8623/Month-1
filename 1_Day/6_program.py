8c48767619de3b897eaa46bb4bec4fc9b9c0b9ab
harshad8623 <dhuppeh@gmail.com>
2026-06-01 11:39:23 +0530

    Auto-update on 01-06-2026 at 11:39:23.62

---
+++ 1_Day/6_program.py
@@ -0,0 +1,5 @@
+# Convert an integer to float and print
+a = 10
+print(f"Before conversion : {a} and its type is : {type(a)}")
+a = float(a)
+print(f"After conversion : {a} and its type is : {type(a)}")
---
+++ 1_Day/7_program.py
@@ -0,0 +1,5 @@
+# Convert a string '42' to integer and add 8
+string_num = "42"
+int_num = int(string_num)
+result = int_num + 8
+print(f"The result of adding 8 to the integer value of '{string_num}' is: {result}")
