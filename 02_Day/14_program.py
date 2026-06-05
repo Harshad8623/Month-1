10e24745ff54b9cde730332db83749a98ecd34e2
harshad8623 <dhuppeh@gmail.com>
2026-06-05 15:44:37 +0530

    Auto-update on 05-06-2026 at 15:44:37.25

---
+++ 02_Day/14_program.py
@@ -0,0 +1,7 @@
+st = input("Enter (yes or no) : ").lower()
+if st == "yes":
+    print("You entered Yes")
+elif st == "no":
+    print("You entered No")
+else:
+    print("Invalid input. Please enter 'yes' or 'no'.")
