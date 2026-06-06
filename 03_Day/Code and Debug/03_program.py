bea960f46a0e12ebba59d9507f99470539426862
harshad8623 <dhuppeh@gmail.com>
2026-06-06 23:39:35 +0530

    Auto-update on 06-06-2026 at 23:39:35.10

---
+++ 03_Day/Code and Debug/03_program.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a Number : "))
+if num <= 0:
+    print("Please enter a positive number greater than zero.")
+else:
+    for i in range(1, num + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
