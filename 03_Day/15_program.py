c01949b8e83443981772c634863993a89ad33da0
harshad8623 <dhuppeh@gmail.com>
2026-06-07 12:05:55 +0530

    Auto-update on 07-06-2026 at 12:05:54.94

---
+++ 03_Day/15_program.py
@@ -0,0 +1,13 @@
+password = ""
+while password != "9923":
+    password = input("Enter Password : ")
+print("Welcome")
+
+
+# or
+
+while True:
+    password = input("Enter Password : ")
+    if password == "9923":
+        break
+print("Welcome")
