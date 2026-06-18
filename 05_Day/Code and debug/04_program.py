e83c4cd0bd3881c49c9cd29cafa8119322ee5c26
harshad8623 <dhuppeh@gmail.com>
2026-06-18 11:40:56 +0530

    Auto-update on 18-06-2026 at 11:40:56.63

---
+++ 05_Day/Code and debug/04_program.py
@@ -0,0 +1,5 @@
+def rectangle_area(length, breadth):
+    return length * breadth
+length = int(input("Enter length : "))
+breadth = int(input("Enter Breadth : "))
+print("Area is",rectangle_area(length, breadth))
