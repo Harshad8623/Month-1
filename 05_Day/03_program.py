a429dc973bbe597433c960eca3a53c82e675e908
harshad8623 <dhuppeh@gmail.com>
2026-06-18 15:23:34 +0530

    Auto-update on 18-06-2026 at 15:23:34.42

---
+++ 05_Day/03_program.py
@@ -0,0 +1,7 @@
+def is_even(num):
+    if num % 2 == 0:
+        return "Even"
+    return "Odd"
+
+print(is_even(10))
+print(is_even(7))
---
+++ 05_Day/04_program.py
@@ -0,0 +1,8 @@
+def max_of_two(a, b):
+    if a > b:
+        return a
+    return b
+
+print(max_of_two(10, 15))
+print(max_of_two(-18, 2))
+print(max_of_two(-12, -28))
